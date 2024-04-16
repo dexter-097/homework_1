51f735b0d22f678bcb67af3d7ce07693679b0d0e
dexter <arturyongh2o@gmail.com>
2024-04-16 13:31:27 +0500

    # решил задачу

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 text = input('введите строку: ')
-print(text[::-1])
+first = text.find(('h'))
+last = text.rfind(('h'))
+print(last)
+new_text = ''
+for i in range(len(text)):
+    if i != first and i != last:
+        if text[i] == 'h':
+            new_text += 'H'
+    new_text += text[i]
+print(new_text)
