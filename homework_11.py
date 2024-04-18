94eb6b22bfe678b2b48e0cb60c648ec3ec221c3e
dexter <arturyongh2o@gmail.com>
2024-04-19 01:41:13 +0500

    #11 решил задачу 11

---
+++ homework_11.py
@@ -0,0 +1,7 @@
+date_dict = {'year': 2024,
+             'month': 4,
+             'day': 14}
+
+result = str(date_dict['year']) + '-' + str(date_dict['month']) + '-' + str(date_dict['day'])
+
+print(result)
